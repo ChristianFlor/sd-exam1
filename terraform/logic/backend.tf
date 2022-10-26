277e626528a53601fc5c53e1de6369f5073fe042
ML7 <thetraceurs@gmail.com>
2022-10-26 18:32:13 -0500

    Added rule

--- terraform/logic/backend.tf
+++ terraform/logic/backend.tf
@@ -34,6 +34,18 @@ resource "azurerm_network_security_group" "nsg_back" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
